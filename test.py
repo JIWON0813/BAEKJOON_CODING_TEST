7b2f49e2a3cb23ec1a30c1ce0a670c8c4d9fb6ba
JIWON KIM <kkjim1324@naver.com>
2022-08-31 22:36:27 +0900

    feat : 체스판 다시 칠하기

--- test.py
+++ test.py
@@ -3,8 +3,53 @@ import os
 import random
 import re
 import sys
-import requests
 import json
 
 
+a , b = map(int,input().split())
 
+list = []
+
+for _ in range(a):
+    list.append(input())
+
+result = sys.maxsize
+for i in range(a-7):
+    for j in range(b-7):
+        start = 'B'
+        wCount = 0
+        bCount = 0
+        for line in list[i:i+8]:
+            for ch in line[j:j+8]:
+                if ch != start:
+                    bCount += 1
+
+                if start == 'B':
+                    start = 'W'
+                else:
+                    start = 'B'
+
+            if start == 'B':
+                start = 'W'
+            else:
+                start = 'B'
+
+        start = 'W'
+        for line in list[i:i+8]:
+            for ch in line[j:j+8]:
+                if ch != start:
+                    wCount += 1
+
+                if start == 'B':
+                    start = 'W'
+                else:
+                    start = 'B'
+
+            if start == 'B':
+                start = 'W'
+            else:
+                start = 'B'
+
+        result = min(result,(min(wCount, bCount)))
+
+print(result)
